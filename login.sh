93a8e95467626594a6d49c27e8c19fe17dc3d972
Valshem3 <valshem2@gmail.com>
2023-08-16 19:43:48 +0000

    this is my first commit

---
+++ login.sh
@@ -0,0 +1,8 @@
+echo "# Team33-G5" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Valshem3/Team33-G5.git
+git push -u origin main
+i am an engineer
